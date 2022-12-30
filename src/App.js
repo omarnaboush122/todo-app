33a233dd3a33b2aba68f174174def2ca7c27110c
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-29 19:09:43 -0800

    create intro component and style it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Intro from "./components/Intro";
+import topBlob from "./images/blob 5 (1).png";
+import bottomBlob from "./images/blob 5.png";
 
+// https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple
 
 const App = () => {
+  const [startQuiz,setStartQuiz] = useState(false);
+
   return (
-    <div>
-      Quiz App
+    <div className="container">
+      <Intro/>
+        <div className="top">
+        <img src={topBlob} alt="top-blob" />
+      </div>
+      <div className="bottom">
+        <img src={bottomBlob} alt="bottom-blob" />
+      </div>
     </div>
   );
 }
