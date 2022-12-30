7fb34f9998f78d0e23b6a6dae93a245e9a85b661
omarnaboush122 <omarnaboush89@gmail.com>
2022-12-29 19:23:31 -0800

    get data from api and create new array for all data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Intro from "./components/Intro";
 import topBlob from "./images/blob 5 (1).png";
 import bottomBlob from "./images/blob 5.png";
 
-// https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple
+
 
 const App = () => {
   const [startQuiz,setStartQuiz] = useState(false);
+  const [questionsArray,setQuestionsArray] = useState([]);
+
+  useEffect(()=> {
+    fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple")
+    .then(res => res.json())
+    .then(data => convertArrayToObj(data.results))
+    .then(results => setQuestionsArray(results));
+  },[])
+
+  function convertArrayToObj(array) {
+    return array.map(item => {
+      return {
+        ...item,
+        allAnswers: [item.correct_answer,...item.incorrect_answers],
+        isHeld: false
+      }
+    })
+  }
 
   return (
     <div className="container">
---
+++ src/components/Questions.js
@@ -0,0 +1,11 @@
+
+
+const Questions = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Questions;
