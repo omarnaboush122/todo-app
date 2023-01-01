b0431bb0116fc5b655e1d41c0ef53c780c9b2429
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-01 13:14:22 -0800

    add completed actions for tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 
 
 const Task = ({mode,text,deleteTask}) => {
+  const [taskCompleted,setTaskCompleted] = useState(true);
+
+  function toggleCompleted() {
+    setTaskCompleted(prevTaskCompleted => !prevTaskCompleted);
+  }
   return (
     <div className={`task ${mode}`}>
-      <div className="circle add"></div>
+      <div className={`circle ${taskCompleted && "completed"}`} onClick={toggleCompleted}></div>
       <div className="text">{text}</div>
       <div className="delete">
         <img src="./images/icon-cross.svg" alt="cross" onClick={deleteTask} />
