c93c0c8d311b888faa1d78fce9cb61c315514037
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-15 08:43:43 -0800

    create context file

--- src/App.js
+++ src/App.js
@@ -1,92 +1,13 @@
-import { useEffect, useState } from "react";
+
 import NewTask from "./components/NewTask";
 import TasksContainer from "./components/TasksContainer";
 import "./App.css";
-import { nanoid } from "nanoid";
+import { useContext } from "react";
+import { Context } from "./Context";
 
 const App = () => {
-  const [tasksArray, setTasksArray] = useState([]);
-  const [mode, setMode] = useState("dark");
-  const [inputText, setInputText] = useState("");
-  const [filteredTasks, setFilteredTasks] = useState([]);
-  const [taskStatus, setTaskStatus] = useState("");
-  const [selectedBtn, setSelectedBtn] = useState("");
-
-  useEffect(() => {
-    setTasksArray(JSON.parse(localStorage.getItem("tasksArray")));
-    setMode(JSON.parse(localStorage.getItem("mode")));
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
-    localStorage.setItem("mode", JSON.stringify(mode));
-  }, [tasksArray, mode]);
-
-  useEffect(() => {
-    const filterHandler = () => {
-      switch (taskStatus) {
-        case "completed":
-          setFilteredTasks(
-            tasksArray.filter((task) => task.completed === true)
-          );
-          setSelectedBtn(3);
-          break;
-        case "active":
-          setFilteredTasks(
-            tasksArray.filter((task) => task.completed === false)
-          );
-          setSelectedBtn(2);
-          break;
-        default:
-          setFilteredTasks(tasksArray);
-          setSelectedBtn(1);
-          break;
-      }
-    };
-
-    filterHandler();
-  }, [tasksArray, taskStatus]);
-
-  function toggleCompleted(id) {
-    setTasksArray((prevTasksArray) => {
-      return prevTasksArray.map((task) => {
-        return task.id === id ? { ...task, completed: !task.completed } : task;
-      });
-    });
-  }
-
-  function toggleMode() {
-    if (mode === "dark") {
-      setMode("light");
-    } else {
-      setMode("dark");
-    }
-  }
-
-  function addTask(taskText) {
-    const newTask = {
-      text: taskText,
-      id: nanoid(),
-      completed: false,
-    };
-    if (taskText !== "") {
-      setTasksArray((prevTasksArray) => [newTask, ...prevTasksArray]);
-      setInputText("");
-    } else {
-      alert("please type something in the form");
-    }
-  }
-
-  function deleteTask(id) {
-    setTasksArray((prevTasksArray) =>
-      prevTasksArray.filter((task) => task.id !== id)
-    );
-  }
-
-  function deleteCompletedTasks() {
-    setTasksArray(tasksArray.filter((task) => task.completed !== true));
-  }
-
+  const {mode,toggleMode} = useContext(Context);
+  
   return (
     <div className={`container ${mode}`}>
       <div className={`header ${mode}`}>
@@ -98,22 +19,8 @@ const App = () => {
             onClick={toggleMode}
           />
         </div>
-        <NewTask
-          mode={mode}
-          addTask={addTask}
-          inputText={inputText}
-          setInputText={setInputText}
-        />
-        <TasksContainer
-          mode={mode}
-          tasksArray={tasksArray}
-          deleteTask={deleteTask}
-          toggleCompleted={toggleCompleted}
-          filteredTasks={filteredTasks}
-          selectedBtn={selectedBtn}
-          setTaskStatus={setTaskStatus}
-          deleteCompletedTasks={deleteCompletedTasks}
-        />
+        <NewTask/>
+        <TasksContainer/>
       </div>
     </div>
   );
---
+++ src/Context.js
@@ -0,0 +1,98 @@
+import { createContext, useEffect, useState} from "react";
+import { nanoid } from "nanoid";
+
+export const Context = createContext();
+
+const ContextProvider = ({children}) => {
+
+  const [tasksArray, setTasksArray] = useState([]);
+  const [mode, setMode] = useState("dark");
+  const [inputText, setInputText] = useState("");
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [taskStatus, setTaskStatus] = useState("");
+  const [selectedBtn, setSelectedBtn] = useState("");
+
+  useEffect(() => {
+    setTasksArray(JSON.parse(localStorage.getItem("tasksArray")));
+    setMode(JSON.parse(localStorage.getItem("mode")));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
+    localStorage.setItem("mode", JSON.stringify(mode));
+  }, [tasksArray, mode]);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch (taskStatus) {
+        case "completed":
+          setFilteredTasks(
+            tasksArray.filter((task) => task.completed === true)
+          );
+          setSelectedBtn(3);
+          break;
+        case "active":
+          setFilteredTasks(
+            tasksArray.filter((task) => task.completed === false)
+          );
+          setSelectedBtn(2);
+          break;
+        default:
+          setFilteredTasks(tasksArray);
+          setSelectedBtn(1);
+          break;
+      }
+    };
+
+    filterHandler();
+  }, [tasksArray, taskStatus]);
+
+  function toggleCompleted(id) {
+    setTasksArray((prevTasksArray) => {
+      return prevTasksArray.map((task) => {
+        return task.id === id ? { ...task, completed: !task.completed } : task;
+      });
+    });
+  }
+
+  function toggleMode() {
+    if (mode === "dark") {
+      setMode("light");
+    } else {
+      setMode("dark");
+    }
+  }
+
+  function addTask(taskText) {
+    const newTask = {
+      text: taskText,
+      id: nanoid(),
+      completed: false,
+    };
+    if (taskText !== "") {
+      setTasksArray((prevTasksArray) => [newTask, ...prevTasksArray]);
+      setInputText("");
+    } else {
+      alert("please type something in the form");
+    }
+  }
+
+  function deleteTask(id) {
+    setTasksArray((prevTasksArray) =>
+      prevTasksArray.filter((task) => task.id !== id)
+    );
+  }
+
+  function deleteCompletedTasks() {
+    setTasksArray(tasksArray.filter((task) => task.completed !== true));
+  }
+
+
+  return (
+    <Context.Provider value={{mode,toggleMode}}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+export default ContextProvider;
